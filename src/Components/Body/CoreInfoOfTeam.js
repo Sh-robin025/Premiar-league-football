38c391713b786a365ae3a770ab5daf450653ae75
Shahadat Robin <sh.robin025@gmail.com>
2021-03-11 06:44:34 +0600

    condition added

--- src/Components/Body/CoreInfoOfTeam.js
+++ src/Components/Body/CoreInfoOfTeam.js
@@ -4,20 +4,28 @@ import foundIco from '../../Icon/found 1.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFutbol, faMars } from '@fortawesome/free-solid-svg-icons';
 import { Container } from 'react-bootstrap';
+import femaleImg from '../../images/female-cooking.png';
+import DescriptionOfTeam from './DescriptionOfTeam';
+import Connection from './Connection';
 
 
 const CoreInfoOfTeam = (props) => {
     const { strTeam, intFormedYear, strCountry, strStadiumLocation, strGender, strTeamFanart4 } = props.data;
-
-    return (
-        <Container>
+    const female = props.female;
+    const femaleInfo = female ?
+        <div className="text-light p-5 core-data">
+            <h3>The Developer Has Nothing To Show Details About Female Team of {strTeam} !</h3>
+            <br />
+            <h4>Female Players Are Busy With Family....</h4>
+            <img src={femaleImg} style={{ width: '100%', height: '350px', borderRadius: '15px', overflow: 'hidden' }} alt="" />
+        </div>
+        : <div>
             <div className="text-light p-4 core-data row justify-content-between">
                 <div className="col">
                     <h2>{strTeam}</h2>
                     <br />
                     <h6>
-                        <i><img style={{ width: '24px', marginRight: '8px' }} src={foundIco} alt="" /></i>
-                    Founded : {intFormedYear}</h6>
+                        <i><img style={{ width: '24px', marginRight: '8px' }} src={foundIco} alt="" /></i> Founded : {intFormedYear}</h6>
                     <h6>
                         <FontAwesomeIcon style={{ marginRight: '8px', fontSize: '20px' }} icon={faFutbol} /> Country : {strCountry}
                     </h6>
@@ -28,9 +36,16 @@ const CoreInfoOfTeam = (props) => {
                         <FontAwesomeIcon style={{ marginRight: '8px', fontSize: '25px' }} icon={faMars} /> Gender : {strGender} </h6>
                 </div>
                 <div style={{ borderRadius: '15px', overflow: 'hidden' }}>
-                    <img src={strTeamFanart4} style={{ height: '250px'}} alt="" />
+                    <img src={strTeamFanart4} style={{ height: '250px' }} alt="" />
                 </div>
             </div>
+            <DescriptionOfTeam data={props.data} />
+        </div>
+
+    return (
+        <Container>
+            {femaleInfo}
+            <Connection data={props.data} />
         </Container>
     );
 };
--- src/Components/Body/TeamDetails.js
+++ src/Components/Body/TeamDetails.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import '../../App.css';
 import { useParams } from 'react-router';
 import BannerOfDetails from './BannerOfDetails';
+import { Button, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFemale } from '@fortawesome/free-solid-svg-icons';
 import CoreInfoOfTeam from './CoreInfoOfTeam';
-import DescriptionOfTeam from './DescriptionOfTeam';
-import Connection from './Connection';
 
 const TeamDetails = () => {
     const { teamId } = useParams()
+    const [female, setFemale] = useState(false);
     const [details, setDetails] = useState({})
     const { strTeamBanner, strTeamBadge, strStadium } = details;
 
@@ -17,13 +19,16 @@ const TeamDetails = () => {
             .then(data => setDetails(data.teams[0]))
     }, [teamId])
 
+
     return (
         <div>
             <BannerOfDetails logo={strTeamBadge} stadium={strStadium} />
-            <div className="details pt-5">
-                <CoreInfoOfTeam data={details} />
-                <DescriptionOfTeam data={details} />
-                <Connection data={details}/>
+            <div className="details pt-3">
+                <Container className="text-center mb-3">
+                    <Button onClick={() => setFemale(!female)}>
+                        <FontAwesomeIcon icon={faFemale} /> Female Team</Button>
+                </Container>
+                <CoreInfoOfTeam data={details} female={female} />
                 <img src={strTeamBanner} className="banner" alt="" />
             </div>
         </div>
